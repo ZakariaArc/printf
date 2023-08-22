c5d30050090c6da8d3e3e9b92fe5e69049f852d9
Atif-dija <khadijaaif2@gmail.com>
2023-08-22 12:04:07 -0700

    Task 0

---
+++ _printf.c
@@ -0,0 +1,78 @@
+#include "main.h"
+#include <stdarg.h>
+#include <unistd.h>
+#include <string.h>
+/**
+ * print_format - handles printing based on format specifier
+ * @format: the format specifier
+ * @args: the variable argument list
+ * Return: the number of characters printed
+ */
+
+int print_format(const char *format, va_list args)
+{
+	int count = 0;
+
+	if (*format)
+	{
+		if (*format == 'c')
+		{
+			char c = (char)va_arg(args, int);
+
+			count += write(1, &c, 1);
+		}
+		else if (*format == 's')
+		{
+			char *str = va_arg(args, char *);
+
+			if (str == NULL)
+				str = "(null)";
+			count += write(1, str, strlen(str));
+		}
+		else if (*format == '%')
+			count += write(1, "%", 1);
+		else
+		{
+			write(1, format - 1, 1);
+			count++;
+			write(1, format, 1);
+			count++;
+		}
+	}
+	return (count);
+}
+
+/**
+ * _printf - the custom printf function
+ * @format: a format string
+ * @...: the variable arguments
+ * Return: the number of characters printed
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int count = 0;
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(args, format);
+
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			count += print_format(format, args);
+		}
+		else
+		{
+			write(1, format, 1);
+			count++;
+		}
+		format++;
+	}
+	va_end(args);
+	return (count);
+}
