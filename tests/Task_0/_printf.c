95c6f807e2bca84524a04079aaae5388b2cb701c
Atif-dija <khadijaaif2@gmail.com>
2023-08-22 08:17:24 -0700

    I'm not going anywhere. You can print that wherever you want to. I'm here and I'm a Spur for life

--- tests/Task_0/_printf.c
+++ tests/Task_0/_printf.c
@@ -12,30 +12,31 @@ int print_format(const char *format, va_list args)
 {
 	int count = 0;
 
-	switch (*format)
+	if (*format)
 	{
-		case 'c':
-			{
-				char c = (char)va_arg(args, int);
+		if (*format == 'c')
+		{
+			char c = (char)va_arg(args, int);
 
-				count += write(1, &c, 1);
-			}
-			break;
-		case 's':
-			{
-				char *str = va_arg(args, char *);
+			count += write(1, &c, 1);
+		}
+		else if (*format == 's')
+		{
+			char *str = va_arg(args, char *);
 
-				if (str == NULL)
-					str = "(null)";
-				count += write(1, str, strlen(str));
-			}
-			break;
-		default:
+			if (str == NULL)
+				str = "(null)";
+			count += write(1, str, strlen(str));
+		}
+		else if (*format == '%')
+			count += write(1, "%", 1);
+		else
+		{
 			write(1, format - 1, 1);
 			count++;
 			write(1, format, 1);
 			count++;
-			break;
+		}
 	}
 	return (count);
 }
